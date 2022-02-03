230e39ac7c6e418f9e9fe390ac1093ce0e6b3885
wn <csignore@whril.com>
2022-02-03 02:23:50 -0500

    copy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Thick of Things 🍌</h1>
+      <p>Updates coming soon, stay tuned!</p>
     </div>
   );
 }
